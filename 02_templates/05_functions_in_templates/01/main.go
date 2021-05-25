1372495ca73e3f3bab6059613ef6ab2b92a1c8a2
neoyewchuan <neoyewchuan@gmail.com>
2021-05-25 14:53:04 +0800

    you're doing great

--- 02_templates/05_functions_in_templates/01/main.go
+++ 02_templates/05_functions_in_templates/01/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
-	"html/template"
 	"log"
 	"os"
+	"strings"
+	"text/template"
 )
+
+var tpl *template.Template
 
 type sage struct {
 	Name  string
@@ -17,15 +20,35 @@ type car struct {
 	Doors        int
 }
 
-var tpl *template.Template
+type items struct {
+	Wisdom    []sage
+	Transport []car
+}
+
+// create a FuncMap to register functions
+// 'uc' is what the func will be called in the template
+// 'uc' is the ToUpper func from package strings
+// 'ft' is a func I declared
+// 'ft' slices a string, returning the first three characters
+var fm = template.FuncMap{
+	"uc": strings.ToUpper,
+	"ft": firstThree,
+}
 
 func init() {
-	tpl = template.Must(template.ParseFiles("tpl.gohtml"))
+	tpl = template.Must(template.New("").Funcs(fm).ParseFiles("tpl.gohtml"))
+	// not working if Funcs is defined later
+	//tpl = template.Must(template.ParseFiles("tpl.gohtml"))
+	//tpl = tpl.Funcs(fm)
+}
+
+func firstThree(s string) string {
+	s = strings.TrimSpace(s)
+	s = s[:3]
+	return s
 }
 
 func main() {
-	//sages := []string{"Gandhi", "MLK", "Buddha", "Jesus", "Muhammad", "LaoTze", "Confusius"}
-
 	buddha := sage{
 		Name:  "Buddha",
 		Motto: "The belief of no beliefs",
@@ -65,17 +88,13 @@ func main() {
 	sages := []sage{buddha, gandhi, mlk, jesus, muhammad}
 	cars := []car{f, t}
 
-	data := struct {
-		Wisdom    []sage
-		Transport []car
-	}{
-		sages,
-		cars,
+	data := items{
+		Wisdom:    sages,
+		Transport: cars,
 	}
 
-	err := tpl.Execute(os.Stdout, data)
+	err := tpl.ExecuteTemplate(os.Stdout, "tpl.gohtml", data)
 	if err != nil {
 		log.Fatalln(err)
 	}
-
 }
